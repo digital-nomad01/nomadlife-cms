a7c6fc661ef0ec4ef57daf5418b29e1c36f750fa
Rajeshwory Shrestha <shrestharajeshwory@gmail.com>
2025-07-01 14:38:11 -0400

    refactor:refactor code

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,9 +4,8 @@ import BlogForm from '@/features/blog/ui/blog-form';
 
 export default function BlogPage() {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8">Blog</h1>
-      <BlogForm />
+    <div>
+      <h1>Blog</h1>
     </div>
   );
 } 
