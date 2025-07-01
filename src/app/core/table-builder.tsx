8b99d7ca1b0590525bbef62cfde8ea976e3cc7a2
Rajeshwory Shrestha <shrestharajeshwory@gmail.com>
2025-07-01 14:39:37 -0400

    chore: add table component

--- src/app/core/table-builder.tsx
+++ src/app/core/table-builder.tsx
@@ -2,3 +2,14 @@ import React from "react";
 import {z} from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+export const TableBuilder = () => {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr></tr>
+                </thead>
+            </table>
+        </div>
+    )
+}
